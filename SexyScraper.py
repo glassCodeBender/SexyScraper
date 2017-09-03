84cff95267892b2768b62bb42447ecbc687ca1d4
J. Alexander <xandermoorman@gmail.com>
2017-09-03 10:47:27 -0500

    A Ghetto Web-Scraper. Added time.sleep to speed up program.

--- SexyScraper.py
+++ SexyScraper.py
@@ -8,6 +8,10 @@ This is a program I wrote to scrape a web-site for information about Windows pro
 Since I don't like python anymore, I'm going to parse the results of the web-scraping from 
 a txt file into a TreeMap with scala.
 
+This program runs EXTREMELY slow. My recommendation is to take the logic, break up the list 
+returned by remove_extras(), and run the program with a bunch of different programs. This assumes
+you don't already know how to write programs with concurrency.
+
 NOTE: I can't remember how to write full python programs anymore so I doubt the program will
 run as is. If you want to use the program, use the program's logic in a jupyter notebook. It'd
 probably just be easier to wait until ProcessTree.scala is finished if you are looking for a 
@@ -58,7 +62,7 @@ class SexyScraper:
         urls = links
         for link in urls:
             try:
-                time.sleep(2)
+                time.sleep(1)
                 url = "http://www.liutilities.com" + link
                 html = urlopen(url)
                 soup = BeautifulSoup(html, "lxml")
