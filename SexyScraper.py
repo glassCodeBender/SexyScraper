723ab5074a093653884a81a7764e1b075da0fd10
J. Alexander <xandermoorman@gmail.com>
2017-09-03 00:34:30 -0500

    A ghetto web-scraper I wrote as fast as possible. It's my first.

--- SexyScraper.py
+++ SexyScraper.py
@@ -4,8 +4,13 @@ import string
 
 """
 This is a program I wrote to scrape a web-site for information about Windows processes.
-Since I don't like python anymore, I'm going to parse the results of the web-scraping into
-a TreeMap with scala.
+Since I don't like python anymore, I'm going to parse the results of the web-scraping from 
+a txt file into a TreeMap with scala.
+
+NOTE: I can't remember how to write full python programs anymore so I doubt the program will
+run as is. If you want to use the program, use the program's logic in a jupyter notebook. It'd
+probably just be easier to wait until ProcessTree.scala is finished if you are looking for a 
+big list of processes.
 """
 
 class SexyScraper:
@@ -47,7 +52,7 @@ class SexyScraper:
                 unique.remove(url)
         return unique
 
-    def scrape_pages(links):
+    def scrape_pages(self, links):
         the_text = []
         urls = links
         for link in urls:
