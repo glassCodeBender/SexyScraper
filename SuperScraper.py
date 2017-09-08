2aaf35067cf651700bcf3d7c81c00cf550b80dda
J. Alexander <xandermoorman@gmail.com>
2017-09-08 13:13:45 -0500

    Program uses Python 2.7 and depends on urllib2 and mechanize
    It works

--- SuperScraper.py
+++ SuperScraper.py
@@ -1,53 +1,53 @@
 from AnonBrowser import *
-# from bs4 import BeautifulSoup
-# from urllib.request import urlopen
 from bs4 import BeautifulSoup
 from sys import argv
 
-def get_links():
-    pages = []
-    for number in range(1, argv(1)):
-        browser = AnonBrowser()
-        browser.anonimize()
-        urlUpdate = "http://www.processlibrary.com/en/directory/a/" + str(number)
-        page = browser.open(urlUpdate)
-        html = page.read()
-        soup = BeautifulSoup(html, "lxml")
-        for link in soup.findAll("a", href=True):
-            pages.append(link['href'])
-    return pages
+if len(argv) == 4:
 
-def scrape_pages(links):
+    script, begin_slice, end_slice, length = argv
+    pages = []
+    for number in range(1, int(length)):
+        try:
+            browser = AnonBrowser()
+            browser.anonymize()
+            urlUpdate = "http://www.processlibrary.com/en/directory/a/" + str(number)
+            page = browser.open(urlUpdate)
+            html = page.read()
+
+            soup = BeautifulSoup(html, "lxml")
+
+            for link in soup.findAll("a", href=True):
+                pages.append(link['href'])
+        except Exception:
+            continue
+
     the_text = []
-    urls = links
     i = 0
+    unique = set(pages)
+    convert_list = list(unique)
+    sorted_list = sorted(convert_list)
+    urls = sorted_list[int(begin_slice):int(end_slice)]
+
     for link in urls:
         try:
             url = "http://www.processlibrary.com/" + link
-            browser = AnonBrowser()
-            browser.anonimize()
-            page = browser.open(url)
-            html = page.read()
+            new_browser = AnonBrowser()
+            new_browser.anonymize()
+            page_found = new_browser.open(url)
+            html = page_found.read()
             soup = BeautifulSoup(html, "lxml")
             result = soup.findAll("div", {"class": "six columns"})
+
         except Exception:
-            pass
-        if result:
+            continue
+
+        if result != '':
             check = str(result[0].get_text)
             the_text.append(check)
+
         i = i + 1
-    return the_text
 
-def main():
-    pages = get_links()
-    unique = set(pages)
-    sorted_list = sorted(unique)
-    # convert_list = list(sorted_list)
-    sliced = sorted_list[argv(2):argv(3)]
-    scraped = scrape_pages(sliced)
-
-    for found in scraped:
+    for found in the_text:
         print found
-
-if __name__ == '__main__':
-    main()
+else:
+    print "Fuck! You didn't enter the correct number of arguments"
