8b351eace466e0d91d59ea96dd0da32b91a27533
J. Alexander <xandermoorman@gmail.com>
2017-09-08 13:37:12 -0500

    Updated Scraper to Automate with Scala code

--- SuperScraper.py
+++ SuperScraper.py
@@ -2,51 +2,60 @@ from AnonBrowser import *
 from bs4 import BeautifulSoup
 from sys import argv
 
-if len(argv) == 4:
-    script, begin_slice, end_slice, length = argv
-    pages = []
-    for number in range(1, int(length)):
-        try:
-            browser = AnonBrowser()
-            browser.anonymize()
-            urlUpdate = "http://www.processlibrary.com/en/directory/a/" + str(number)
-            page = browser.open(urlUpdate)
-            html = page.read()
+"""
+Program uses Python 2.7. Has mechanize and urllib2 dependencies. 
+"""
 
-            soup = BeautifulSoup(html, "lxml")
+script, begin_slice, end_slice, length, output_file = argv
+pages = []
+for number in range(1, int(length)):
+    try:
+        browser = AnonBrowser()
+        browser.anonymize()
+        urlUpdate = "http://www.processlibrary.com/en/directory/a/" + str(number)
+        page = browser.open(urlUpdate)
+        html = page.read()
 
-            for link in soup.findAll("a", href=True):
-                pages.append(link['href'])
-        except Exception:
-            continue
+        soup = BeautifulSoup(html, "lxml")
 
-    the_text = []
-    i = 0
-    unique = set(pages)
-    convert_list = list(unique)
-    sorted_list = sorted(convert_list)
-    urls = sorted_list[int(begin_slice):int(end_slice)]
+        for link in soup.findAll("a", href=True):
+            pages.append(link['href'])
+    except Exception:
+        continue
 
-    for link in urls:
-        try:
-            url = "http://www.processlibrary.com/" + link
-            new_browser = AnonBrowser()
-            new_browser.anonymize()
-            page_found = new_browser.open(url)
-            html = page_found.read()
-            soup = BeautifulSoup(html, "lxml")
-            result = soup.findAll("div", {"class": "six columns"})
+the_text = []
+i = 0
+unique = set(pages)
+convert_list = list(unique)
+sorted_list = sorted(convert_list)
+urls = sorted_list[int(begin_slice):int(end_slice)]
 
-        except Exception:
-            continue
+for link in urls:
+    try:
+        url = "http://www.processlibrary.com/" + link
+        new_browser = AnonBrowser()
+        new_browser.anonymize()
+        page_found = new_browser.open(url)
+        next_html = page_found.read()
+        next_soup = BeautifulSoup(next_html, "lxml")
+        result = next_soup.findAll("div", {"class": "six columns"})
 
-        if result != '':
-            check = str(result[0].get_text)
-            the_text.append(check)
+    except Exception:
+        continue
 
-        i = i + 1
+    if result != '':
+        check = str(result[0].get_text)
+        the_text.append(check)
 
-    for found in the_text:
-        print found
-else:
-    print "Fuck! You didn't enter the correct number of arguments"
+    i = i + 1
+
+for found in the_text:
+    print found
+
+try:
+    the_file = open(output_file, "w")
+    for line in the_text:
+        the_file.write(line)
+    the_file.close()
+except IOError:
+    print "Something went wrong while printing to file."
